3fd54e0106873d18c0ddf6a6ffe0ce8527a4b661
Denis Tkachev <allpost-s@ya.ru>
2024-09-23 15:37:29 +0300

    function_recursion is going

---
+++ 3_functions/31_higherOrderFunction/index.js
@@ -0,0 +1,28 @@
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
+  }
+  return newArray;
+}
+const numbers = [1, 2, 3, 4, 5];
+
+function isOdd(num) { /**Проверка на нечетность -> true / false */
+  // Ваш код здесь
+  if (num % 2 !== 0) {
+    return true;
+  } else return false;
+}
+function isEven(num) { /**Проверка на четность -> true / false */
+  // Ваш код здесь
+  if (num % 2 === 0) {
+    return true;
+  } else return false;
+}
+
+// console.log(isOdd(4));
+// console.log(isEven(5));
+
+// Для проверки используйте логи (раскомментируйте их после написания функций)
+console.log(filterArray(numbers, isEven)); // Должен вывести: [2, 4]
+console.log(filterArray(numbers, isOdd)); // Должен вывести: [1, 3, 5]
