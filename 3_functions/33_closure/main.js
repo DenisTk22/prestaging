f4f8b25743ee875202df009593dd317339071f78
Denis Tkachev <allpost-s@ya.ru>
2024-09-24 20:18:26 +0300

    clouser is done

--- 3_functions/33_closure/main.js
+++ 3_functions/33_closure/main.js
@@ -10,13 +10,18 @@ addByFour(3); // 7
 addByFour(5); // 9
  */
 
-function addByX(x) {
-    function add(a) {return x + a;}
-    return add(2);
+function addByX(a) {
+    function sum(b) {return a + b;}
+    return sum;
 }
 
-const addByTwo = addByX(2);
-addByTwo(3); // 5
-addByTwo(5); // 7
+const addByTwo = addByX(2); /**Функция addByX принимает число в качестве параметра а */
+addByTwo(3); // 5 /**Функция addByTwo принимает число в качестве парамера b */
 
 console.log(addByTwo(3))
+console.log(addByTwo(5))
+
+const addByFour = addByX(4);
+
+console.log(addByFour(3))
+console.log(addByFour(5))
