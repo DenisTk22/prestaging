a72b92cbbe57eb93790734375b6cab137553647e
Denis Tkachev <allpost-s@ya.ru>
2024-09-25 15:05:34 +0300

    project in process

---
+++ 3_functions/34_context/main.js
@@ -0,0 +1,20 @@
+// Дан объект supporter1. Опишите его метод chant, при вызове которого с задержкой в 1 секунду в консоль выводится сообщение "Forza Milan".
+// Затем привяжите к объекту supporter2 метод chant таким образом, чтобы при вызове спустя 1 секунду в консоль выводилось сообщение "Forza Inter".
+
+const supporter1 = {
+    club: 'Milan',
+    chant() {
+        setTimeout(() => {
+            console.log("Forza " + this.club)
+        }, 1000);
+    }
+}
+
+const supporter2 = {
+    club: 'Inter',
+}
+
+supporter2.chant = supporter1.chant.bind(supporter2);
+
+supporter1.chant()
+supporter2.chant()
