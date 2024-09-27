07eaf9319c3c53e2d87af0ffc789eb0b733031ef
Denis Tkachev <allpost-s@ya.ru>
2024-09-27 14:41:25 +0300

    preparing for verification

--- 3_functions/31_higherOrderFunction/main.js
+++ 3_functions/31_higherOrderFunction/main.js
@@ -8,20 +8,15 @@ function filterArray(array, callback) {
 const numbers = [1, 2, 3, 4, 5];
 
 function isOdd(num) { /**Проверка на нечетность -> true / false */
-  // Ваш код здесь
   if (num % 2 !== 0) {
     return true;
   } else return false;
 }
 function isEven(num) { /**Проверка на четность -> true / false */
-  // Ваш код здесь
   if (num % 2 === 0) {
     return true;
   } else return false;
 }
-
-// console.log(isOdd(4));
-// console.log(isEven(5));
 
 // Для проверки используйте логи (раскомментируйте их после написания функций)
 console.log(filterArray(numbers, isEven)); // Должен вывести: [2, 4]
