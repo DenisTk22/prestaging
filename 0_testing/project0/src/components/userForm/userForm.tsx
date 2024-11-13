73d4f0fedfb2415e735c2bfeefa6a93d0bd10a9c
Denis Tkachev <allpost-s@ya.ru>
2024-11-13 13:17:24 +0300

    error box

--- 0_testing/project0/src/components/userForm/userForm.tsx
+++ 0_testing/project0/src/components/userForm/userForm.tsx
@@ -1,32 +1,32 @@
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import styles from './userForm.module.css';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import LaunchIcon from '@mui/icons-material/Launch';
 import { NavLink } from "react-router-dom";
-import TextField from '@mui/material/TextField';
+import { ReactElement } from 'react';
 
-  export const SignupForm = () => {
+  export const SignupForm = (): ReactElement => {
+
     return (
     <div className={styles.userForm}>
         <div className={styles.userForm__head}>
           <p className={styles.userForm__title}>Создать пользователя</p>
           <a className={styles.userForm__link} href={'ROUTE_CONSTANTS.ROOT'}>
-            <ExitToAppIcon/>
+            <LaunchIcon/>
           </a>
         </div>
         <Formik
             initialValues={{ user: '', email: '', password: '' }}
             validationSchema={Yup.object({
                 user: Yup.string()
-                    .max(15, 'Не более 15 символов')
-                    .required('Нужно заполнить'),
+                    .max(15, 'Введите не более 15 символов')
+                    .required('Введите имя пользователя'),
                 email: Yup.string()
                     .email('Email не действительный')
-                    .required('Нужно заполнить'), 
+                    .required('Введите Email'), 
                 password: Yup.string()
-                    .min(8, 'Не менее 8 символов')
-                    .required('Нужно заполнить'),
-            
+                    .min(8, 'Введите не менее 8 символов')
+                    .required('Введите пароль'),
             })}
             onSubmit={(values, { setSubmitting }) => {
             setTimeout(() => {
@@ -37,19 +37,34 @@ import TextField from '@mui/material/TextField';
        >
         <Form className={styles.userForm__form}>
             <div className={styles.userForm__inputs}>
-                <Field name="user" type="text" placeholder='Имя пользователя'/>
-                <ErrorMessage className={styles.userFrom__error} name="user" />
-
-                <Field name="email" type="email" placeholder='Email'/>
-                <ErrorMessage className={styles.userFrom__error} name="email" />
-
-                <Field name="password" type="password" placeholder='Пароль'/>
-                <ErrorMessage className={styles.userFrom__error} name="password" />
+                <Field 
+                  name="user" 
+                  type="text" 
+                  placeholder='Имя пользователя'
+                />
+                <div className={styles.userFrom__errorBox}>
+                  <ErrorMessage component="p" className={styles.userFrom__error} name="user" />
+                </div>
+                <Field
+                  name="email" 
+                  type="email" 
+                  placeholder='Email'
+                  />
+                <div className={styles.userFrom__errorBox}>
+                  <ErrorMessage component="p" className={styles.userFrom__error} name="email" />
+                </div>
+                <Field 
+                  name="password" 
+                  type="password"
+                  placeholder='Пароль'
+                  />
+                <div className={styles.userFrom__errorBox}>
+                  <ErrorMessage component="p" className={styles.userFrom__error} name="password" />
+                </div>
             </div>
             <div className={styles.userForm__buttonContainer}>
               <button className={styles.userForm__button} type="submit">Создать</button>
             </div>
-          
         </Form>
       </Formik>
     </div>
