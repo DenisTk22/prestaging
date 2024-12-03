5f87f87b5a25bc496aeec7c1be4028cd780690aa
Denis Tkachev <allpost-s@ya.ru>
2024-12-03 22:12:01 +0300

    async

---
+++ 0_testing/project0/src/async/async.js
@@ -0,0 +1,33 @@
+export const URL = 'https://api.spacexdata.com/v3/cores';
+
+async function getCoreSerial() { // для стрелочной ф: const getUsers = async () => {}
+    const response = await fetch(URL); //await позволяет получить результат работы промиса
+    const data = await response.json();
+    const coreSerial = data.map(core => { return core.core_serial })
+    return coreSerial;
+}
+
+getCoreSerial()
+            .then(console.log)
+            .catch(console.error)
+            .finally(console.log(`Был запрос по адресу: ${URL}`))
+
+const getStatus = new Promise((resolve, reject) => {
+    fetch(URL)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Ошибка ответа сети')
+            } 
+            return response.json()
+        })
+        .then((data) => resolve(data.map(core => { return core.status})))
+        .catch((error) => reject(error));
+});
+
+getStatus
+        .then((data) => {
+            console.log('Статусы:', data);
+        })
+        .catch((error) => {
+            console.error('Ошибка загрузки данных', error);
+        })
